327cb3b3e384d0d42b1da233e13cdc87316241a3
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-09-24 13:53:39 -0400

    added footer

--- nba-register-page/src/App.js
+++ nba-register-page/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/navigation'
 import Register from './components/register'
+import FooterBar from './components/footer'
 
 function App() {
   return (
     <div className="App">
      <NavBar />
      <Register />
+     <FooterBar />
     </div>
   );
 }
--- nba-register-page/src/components/register.js
+++ nba-register-page/src/components/register.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const Form = styled.div `
     width: 600px;
+    height: 88vh;
     margin: 0 auto;
     display: flex;
     flex-direction: column;
